b930d3faae93a3f656c60c2a015c94ddbee02225
Franklin Membreno <frank_aroca1@hotmail.com>
2023-08-24 23:40:31 -0800

    correction in line 53 and comments added in js code

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 // Assignment Code
-const generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Character sets to be used in password generation
-const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numericChars = "0123456789";
-const specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
 
 // Function to generate a random character from a given string
 function getRandomCharacter(characters) {
-  const randomIndex = Math.floor(Math.random() * characters.length);
+  var randomIndex = Math.floor(Math.random() * characters.length);
   return characters[randomIndex];
 }
 
 // Function to generate the password
 function generatePassword() {
-  const password = "";
-  const passwordLength = parseInt(prompt("Enter the desired password length:"));
+  var password = "";
+  var passwordLength = parseInt(prompt("Enter the desired password length:"));
 
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid password length between 8 and 128.");
     return "";
   }
 
-  const useLowercase = confirm("Include lowercase characters?");
-  const useUppercase = confirm("Include uppercase characters?");
-  const useNumeric = confirm("Include numeric characters?");
-  const useSpecial = confirm("Include special characters?");
+  var useLowercase = confirm("Include lowercase characters?");
+  var useUppercase = confirm("Include uppercase characters?");
+  var useNumeric = confirm("Include numeric characters?");
+  var useSpecial = confirm("Include special characters?");
 
- 
   if (!useLowercase && !useUppercase && !useNumeric && !useSpecial) {
     alert("Please select at least one character set to include.");
     return "";
   }
-
+    
   var availableChars = "";
-
+    //validation for every character permitted
+    
   if (useLowercase) {
     availableChars += lowercaseChars;
   }
