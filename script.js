a14dd36f025a175e8188b032aca3b10da2d9bd15
Franklin Membreno <frank_aroca1@hotmail.com>
2023-08-24 23:33:33 -0800

    added function for the password input

--- script.js
+++ script.js
@@ -55,3 +55,11 @@ function generatePassword() {
 
   return password;
 }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
