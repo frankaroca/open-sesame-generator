56c945c26d53bec3878789d6263edb605dbc3084
Franklin Membreno <frank_aroca1@hotmail.com>
2023-08-24 23:30:25 -0800

    variables declaration changed from var to const

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Character sets to be used in password generation
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "0123456789";
-var specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numericChars = "0123456789";
+const specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
 
 // Function to generate a random character from a given string
 function getRandomCharacter(characters) {
-  var randomIndex = Math.floor(Math.random() * characters.length);
+  const randomIndex = Math.floor(Math.random() * characters.length);
   return characters[randomIndex];
 }
 
 // Function to generate the password
 function generatePassword() {
-  var password = "";
-  var passwordLength = parseInt(prompt("Enter the desired password length:"));
+  const password = "";
+  const passwordLength = parseInt(prompt("Enter the desired password length:"));
 
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid password length between 8 and 128.");
     return "";
   }
+
+  const useLowercase = confirm("Include lowercase characters?");
+  const useUppercase = confirm("Include uppercase characters?");
+  const useNumeric = confirm("Include numeric characters?");
+  const useSpecial = confirm("Include special characters?");
 
  
